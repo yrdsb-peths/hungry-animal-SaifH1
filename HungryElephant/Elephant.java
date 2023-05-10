a575eaaafb7a0ac2761e03bc41778a4df6ce2274
SaifH1 <350244810@gapps.yrdsb.ca>
2023-05-10 08:45:06 -0400

    Created world & elephant

---
+++ HungryElephant/Elephant.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Trunk pachyderm.
+ * 
+ * @author Saif
+ * @version (a version number or a date)
+ */
+public class Elephant extends Actor
+{
+    /**
+     * Act - do whatever the elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        move(1);
+    }
+}
