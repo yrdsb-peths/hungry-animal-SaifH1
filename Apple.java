d8da3c361c2a42549a66ef4d49f0e059827a29fb
SaifH1 <130993395+SaifH1@users.noreply.github.com>
2023-05-15 17:32:02 -0400

    ScoreLabel & GameOver added

---
+++ Apple.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Nom nom nom
+ * 
+ * @author SAIF
+ * @version 05/15
+ */
+public class Apple extends Actor
+{
+    /**
+     * Act - do whatever the Apple wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        int x = getX();
+        int y = getY() + 2;
+        setLocation(x, y);
+        
+        MyWorld world = (MyWorld) getWorld();
+        if(getY() >= world.getHeight())
+        {
+            world.gameOver();
+            world.removeObject(this);
+        }
+    }
+}
--- Elephant.java
+++ Elephant.java
@@ -15,6 +15,26 @@ public class Elephant extends Actor
     public void act()
     {
         // Add your action code here.
-        move(1);
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-1);
+        }
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(+1);
+        }
+        
+        eat();
+    }
+    
+    public void eat()
+    {
+        if(isTouching(Apple.class))
+        {
+            removeTouching(Apple.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createApple();
+            world.increaseScore();
+        }
     }
 }
